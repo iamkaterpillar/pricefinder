bf11a30d8474cd6db7c19d1ca10304a065c55470
Kat <107869641+iamkaterpillar@users.noreply.github.com>
2023-05-03 11:04:57 +0100

    tested 0x api

---
+++ 0xapi.py
@@ -0,0 +1,21 @@
+import requests
+import pprint
+
+pp = pprint.PrettyPrinter(indent=1)
+
+# This is us setting up the parameters in Python
+
+params123 = {
+    "sellToken": 'DAI' ,
+    "buyToken": 'WETH' ,
+    "sellAmount": str(100000 * 10**18) ,
+    
+}
+
+resp = requests.get("https://api.0x.org/swap/v1/quote", params=params123, timeout=2.50)
+
+parseresponse = resp.json()
+
+# pp.pprint(parseresponse)
+p=float(parseresponse['price'])
+print(1/p)
